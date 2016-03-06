ee7811f2c2f4edfa673e553ea89e7a869b5bb04c
tainzhi <qfq61@qq.com>
2016-03-06 16:27:47 +0800

    finalize actionbar

--- app/src/main/java/com/qfq/muqing/myvideoplayer/MainActivity.java
+++ app/src/main/java/com/qfq/muqing/myvideoplayer/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         mContext = getApplicationContext();
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME );
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME );
 
         mList = (RecyclerView)findViewById(R.id.selection_list);
         mList.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
