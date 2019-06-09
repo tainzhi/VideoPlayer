b6975cfc198e7a989ec15b21b449afe518fb867a
tainzhi <qfq61@qq.com>
2019-06-09 23:55:17 +0800

    添加启动界面; 主界面重构, 但是目前无法运行demo, 要修复

---
+++ app/src/main/java/com/qfq/tainzhi/videoplayer/mvp/view/activity/impl/ISplashActivityView.java
@@ -0,0 +1,9 @@
+package com.qfq.tainzhi.videoplayer.mvp.view.activity.impl;
+
+/**
+ * Created by muqing on 2019/6/9.
+ * Email: qfq61@qq.com
+ */
+public interface ISplashActivityView {
+    public void enterApp();
+}
---
+++ app/src/main/java/com/qfq/tainzhi/videoplayer/wedget/BottomNavigationBehavior.java
@@ -0,0 +1,25 @@
+package com.qfq.tainzhi.videoplayer.wedget;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+/**
+ * Created by muqing on 2019/6/9.
+ * Email: qfq61@qq.com
+ */
+// public class BottomNavigationBehavior extends CoordinatorLayout.Behavior<View> {
+    // FIXME: 2019/6/9
+    // private ObjectAnimator outAnimator, inAnimator;
+    //
+    // public BottomNavigationBehavior(Context context, AttributeSet attrs) {
+    //     super(context, attrs);
+    // }
+    //
+    // @Override
+    // public boolean
+// }
