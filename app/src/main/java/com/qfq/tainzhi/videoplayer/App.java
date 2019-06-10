996239355f5131c61be4cb5fbd9435e0d443c035
tainzhi <qfq61@qq.com>
2019-06-10 22:45:56 +0800

    本地视频列表界面实现

--- app/src/main/java/com/qfq/tainzhi/videoplayer/App.java
+++ app/src/main/java/com/qfq/tainzhi/videoplayer/App.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
 /**
  * Created by muqing on 2019/6/10.
  * Email: qfq61@qq.com
@@ -23,6 +26,7 @@ public class App extends MultiDexApplication {
         //     SdkManager.initStetho(this);
         //     SdkManager.initLeakCanary(this);
         // }
+        Logger.addLogAdapter(new AndroidLogAdapter());
     }
     
     private void initTheme() {
