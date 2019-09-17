e736357b4975eeaa0893e3689427d5d528200542
tainzhi <qfq61@qq.com>
2019-09-17 09:19:19 +0800

    change permission grant method

--- app/src/main/java/com/qfq/tainzhi/videoplayer/ui/activity/SplashActivity.java
+++ app/src/main/java/com/qfq/tainzhi/videoplayer/ui/activity/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.qfq.tainzhi.videoplayer.ui.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,10 +12,14 @@ import com.qfq.tainzhi.videoplayer.mvp.presenter.SplashPresenter;
 import com.qfq.tainzhi.videoplayer.mvp.presenter.impl.ISplashPresenter;
 import com.qfq.tainzhi.videoplayer.ui.activity.impl.ISplashActivityView;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
 /**
  * Created by muqing on 2019/6/9.
  * Email: qfq61@qq.com
  */
+@RuntimePermissions
 public class SplashActivity extends AppCompatActivity implements ISplashActivityView {
     private ISplashPresenter mSplashPresenter;
     
@@ -25,12 +31,23 @@ public class SplashActivity extends AppCompatActivity implements ISplashActivity
     }
     
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        SplashActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+    @Override
     protected void onDestroy() {
         super.onDestroy();
     }
     
     @Override
     public void enterApp() {
+        SplashActivityPermissionsDispatcher.startActivityWithPermissionCheck(this);
+    }
+    
+    @NeedsPermission({Manifest.permission.READ_EXTERNAL_STORAGE})
+    public void startActivity() {
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
