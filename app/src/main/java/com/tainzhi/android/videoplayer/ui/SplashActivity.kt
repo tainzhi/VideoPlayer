28d2ecf9ddfe4a755396cf717f9e02fd9315adb0
tainzhi <qfq61@qq.com>
2020-05-09 17:03:20 +0800

    使用MaterialDesign主题

---
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.tainzhi.android.videoplayer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
