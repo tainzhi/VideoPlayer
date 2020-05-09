eb46677811d416611e7ef77f502fb42de967b1d5
tainzhi <qfq61@qq.com>
2020-05-09 17:03:33 +0800

    使用MaterialDesign主题

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/SplashActivity.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.tainzhi.android.videoplayer.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.qfq.tainzhi.videoplayer.ui.activity.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
