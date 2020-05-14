83f72cb20973758c5a36692aee04b6ef2973066c
tainzhi <qfq61@qq.com>
2020-05-14 13:36:21 +0800

    feat: 添加权限申请

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/SplashActivity.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/SplashActivity.kt
@@ -1,14 +1,95 @@
 package com.tainzhi.android.videoplayer.ui
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.tainzhi.android.videoplayer.widget.showCheckPermissionDialog
 
 class SplashActivity : AppCompatActivity() {
 
+    companion object {
+        const val REQUEST_REQUIRED_PERMISSIONS = 42
+    }
+
+    private val requestPermissions = arrayOf(
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    private var isMissingPermissions = true
+    private var showRationale = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkPermission()
+        if (!isMissingPermissions) {
+            navigateToMain()
+        }
+    }
+
+    private fun navigateToMain() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
+    }
+
+    /**
+     * 检查是否缺少权限
+     */
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            isMissingPermissions = false
+        } else {
+            requestMissingRequiredPermissions()
+        }
+    }
+
+    private fun requestMissingRequiredPermissions() {
+        val missing = HashSet<String>()
+        showRationale = false
+        for (permission in requestPermissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                missing.add(permission)
+                showRationale = showRationale or (shouldShowRequestPermissionRationale(permission))
+            }
+        }
+        if (missing.isNotEmpty()) {
+            requestPermissions(missing.toTypedArray(), REQUEST_REQUIRED_PERMISSIONS)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_REQUIRED_PERMISSIONS) {
+            var granted = true
+            var _showRationale = false
+
+            for (i in 0 until grantResults.size) {
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                    granted = false
+                    _showRationale = _showRationale || shouldShowRequestPermissionRationale(permissions[i])
+
+                }
+            }
+            if (granted) {
+                navigateToMain()
+            } else {
+                // If we were not supposed to show the rationale before requestPermissions(...) and
+                // we still shouldn't show the rationale it means the user previously selected
+                // "don't ask again" in the permission request dialog. In this case we bring up the
+                // system permission settings for this package.
+                if (!_showRationale && !showRationale) {
+                    showCheckPermissionDialog(supportFragmentManager)
+                } else {
+                    finish()
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 }
---
+++ app/src/main/java/com/tainzhi/android/videoplayer/widget/CheckPermissionDialog.kt
@@ -0,0 +1,46 @@
+package com.tainzhi.android.videoplayer.widget
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/5/14 13:18
+ * @description:
+ **/
+ 
+
+class CheckPermissionDialog: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?) =
+            AlertDialog.Builder(activity)
+                    .setTitle("权限设置")
+                    .setMessage("缺少读取本机视频的权限和访问网络权限, 请到Settings设置")
+                    .setPositiveButton("设置") { _, _ ->
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                .apply {
+                                    data = Uri.fromParts("package", requireActivity().packageName, null)
+                                }
+                        startActivity(intent)
+                        requireActivity().finish()
+                    }
+                    .setNegativeButton("退出") { _, _ ->
+                        requireActivity().finish()
+                    }
+                    .create()
+}
+
+fun Activity.showCheckPermissionDialog(fragmentMamager: FragmentManager) {
+    CheckPermissionDialog().apply  {
+        // arguments = Bundle().apply {
+        //     putString("title", "title")
+        // }
+    }.show(fragmentMamager, "Check Permission")
+}
+
