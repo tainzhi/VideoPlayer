8e71bdcf4599f759496cc4a7f48935f5d2f5cace
tainzhi <qfq61@qq.com>
2020-05-22 14:51:35 +0800

    初步添加module QMediaPlayer

---
+++ QMediaPlayer/src/main/java/com/tanzhi/qmediaplayer/IRenderView.kt
@@ -0,0 +1,51 @@
+package com.tanzhi.qmediaplayer
+
+import android.graphics.SurfaceTexture
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.View
+
+/**
+ * Created by muqing on 2019/6/1.
+ * Email: qfq61@qq.com
+ */
+interface IRenderView {
+    val view: View
+    fun shouldWaitForResize(): Boolean
+    fun setVideoSize(videoWidth: Int, videoHeight: Int)
+    fun setVideoSampleAspectRatio(videoSarNum: Int, videoSarDen: Int)
+    fun setVideoRotation(degree: Int)
+    fun setAspectRatio(aspectRatio: Int)
+    fun addRenderCallback(callback: IRenderCallback)
+    fun removeRenderCallback(callback: IRenderCallback)
+
+    interface ISurfaceHolder {
+        // fun bindToMediaPlayer(mp: MediaPlayer)
+        // fun bindToMediaPlayer(mp: IMediaPlayer)
+        // fun bindToMediaPlayer(mp: ExoPlayer)
+        fun bindToMediaPlayer(mp: IMediaInterface)
+        val renderView: IRenderView
+        val surfaceHolder: SurfaceHolder?
+        fun openSurface(): Surface?
+        val surfaceTexture: SurfaceTexture?
+    }
+
+    interface IRenderCallback {
+        /**
+         * @param holder
+         * @param width  could be 0
+         * @param height could be 0
+         */
+        fun onSurfaceCreated(holder: ISurfaceHolder, width: Int, height: Int)
+
+        /**
+         * @param holder
+         * @param format could be 0
+         * @param width
+         * @param height
+         */
+        fun onSurfaceChanged(holder: ISurfaceHolder, format: Int, width: Int, height: Int)
+
+        fun onSurfaceDestroyed(holder: ISurfaceHolder)
+    }
+}
