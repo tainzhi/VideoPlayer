fe52f4ada4876fc881c96f02051e2c388758e428
tainzhi <qfq61@qq.com>
2020-05-22 14:51:35 +0800

    初步添加module QMediaPlayer

---
+++ QMediaPlayer/src/main/java/com/tanzhi/qmediaplayer/MeasureHelper.kt
@@ -0,0 +1,178 @@
+package com.tanzhi.qmediaplayer
+
+import android.view.View
+import android.view.View.MeasureSpec
+import com.qfq.tainzhi.videoplayer.my_media.Constant.AspectRatio
+import java.lang.ref.WeakReference
+
+/**
+ * Created by muqing on 2019/6/1.
+ * Email: qfq61@qq.com
+ */
+class MeasureHelper(view: View) {
+    private val mWeakView: WeakReference<View>?
+    private var mVideoWidth = 0
+    private var mVideoHeight = 0
+    private var mVideoSarNum = 0
+    private var mVideoSarDen = 0
+    private var mVideoRotationDegree = 0
+    var measuredWidth = 0
+        private set
+    var measuredHeight = 0
+        private set
+    private var mCurrentAspectRatio: Int = AspectRatio.AR_MATCH_PARENT
+    val view: View?
+        get() = mWeakView?.get()
+
+    fun setVideoSize(videoWidth: Int, videoHeight: Int) {
+        mVideoWidth = videoWidth
+        mVideoHeight = videoHeight
+    }
+
+    fun setVideoSampleAspectRatio(videoSarNum: Int, videoSarDen: Int) {
+        mVideoSarNum = videoSarNum
+        mVideoSarDen = videoSarDen
+    }
+
+    fun setVideoRotation(videoRotationDegree: Int) {
+        mVideoRotationDegree = videoRotationDegree
+    }
+
+    /**
+     * Must be called by View.onMeasure(int, int)
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    fun doMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        // logD("${MeasureSpec.toString(widthMeasureSpec)}, ${MeasureSpec.toString(heightMeasureSpec)}")
+        var widthMeasureSpec = widthMeasureSpec
+        var heightMeasureSpec = heightMeasureSpec
+        if (mVideoRotationDegree == 90 || mVideoRotationDegree == 270) {
+            val tempSpec = widthMeasureSpec
+            widthMeasureSpec = heightMeasureSpec
+            heightMeasureSpec = tempSpec
+        }
+        var width = View.getDefaultSize(mVideoWidth, widthMeasureSpec)
+        var height = View.getDefaultSize(mVideoHeight, heightMeasureSpec)
+        if (mCurrentAspectRatio == AspectRatio.AR_MATCH_PARENT) {
+            width = widthMeasureSpec
+            height = heightMeasureSpec
+        } else if (mVideoWidth > 0 && mVideoHeight > 0) {
+            val widthSpecMode = MeasureSpec.getMode(widthMeasureSpec)
+            val widthSpecSize = MeasureSpec.getSize(widthMeasureSpec)
+            val heightSpecMode = MeasureSpec.getMode(heightMeasureSpec)
+            val heightSpecSize = MeasureSpec.getSize(heightMeasureSpec)
+            if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
+                val specAspectRatio = widthSpecSize.toFloat() / heightSpecSize.toFloat()
+                var displayAspectRatio: Float
+                when (mCurrentAspectRatio) {
+                    AspectRatio.AR_16_9_FIT_PARENT -> {
+                        displayAspectRatio = 16.0f / 9.0f
+                        if (mVideoRotationDegree == 90 || mVideoRotationDegree == 270) displayAspectRatio = 1.0f / displayAspectRatio
+                    }
+                    AspectRatio.AR_4_3_FIT_PARENT -> {
+                        displayAspectRatio = 4.0f / 3.0f
+                        if (mVideoRotationDegree == 90 || mVideoRotationDegree == 270) displayAspectRatio = 1.0f / displayAspectRatio
+                    }
+                    AspectRatio.AR_ASPECT_FIT_PARENT, AspectRatio.AR_ASPECT_FILL_PARENT, AspectRatio.AR_ASPECT_WRAP_CONTENT -> {
+                        displayAspectRatio = mVideoWidth.toFloat() / mVideoHeight.toFloat()
+                        if (mVideoSarNum > 0 && mVideoSarDen > 0) displayAspectRatio = displayAspectRatio * mVideoSarNum / mVideoSarDen
+                    }
+                    else -> {
+                        displayAspectRatio = mVideoWidth.toFloat() / mVideoHeight.toFloat()
+                        if (mVideoSarNum > 0 && mVideoSarDen > 0) displayAspectRatio = displayAspectRatio * mVideoSarNum / mVideoSarDen
+                    }
+                }
+                val shouldBeWider = displayAspectRatio > specAspectRatio
+                when (mCurrentAspectRatio) {
+                    AspectRatio.AR_ASPECT_FIT_PARENT, AspectRatio.AR_16_9_FIT_PARENT, AspectRatio.AR_4_3_FIT_PARENT -> if (shouldBeWider) { // too wide, fix width
+                        width = widthSpecSize
+                        height = (width / displayAspectRatio).toInt()
+                    } else { // too high, fix height
+                        height = heightSpecSize
+                        width = (height * displayAspectRatio).toInt()
+                    }
+                    AspectRatio.AR_ASPECT_FILL_PARENT -> if (shouldBeWider) { // not high enough, fix height
+                        height = heightSpecSize
+                        width = (height * displayAspectRatio).toInt()
+                    } else { // not wide enough, fix width
+                        width = widthSpecSize
+                        height = (width / displayAspectRatio).toInt()
+                    }
+                    AspectRatio.AR_ASPECT_WRAP_CONTENT -> if (shouldBeWider) { // too wide, fix width
+                        width = Math.min(mVideoWidth, widthSpecSize)
+                        height = (width / displayAspectRatio).toInt()
+                    } else { // too high, fix height
+                        height = Math.min(mVideoHeight, heightSpecSize)
+                        width = (height * displayAspectRatio).toInt()
+                    }
+                    else -> if (shouldBeWider) {
+                        width = Math.min(mVideoWidth, widthSpecSize)
+                        height = (width / displayAspectRatio).toInt()
+                    } else {
+                        height = Math.min(mVideoHeight, heightSpecSize)
+                        width = (height * displayAspectRatio).toInt()
+                    }
+                }
+            } else if (widthSpecMode == MeasureSpec.EXACTLY && heightSpecMode == MeasureSpec.EXACTLY) { // the size is fixed
+                width = widthSpecSize
+                height = heightSpecSize
+                // for compatibility, we adjust size based on aspect ratio
+                if (mVideoWidth * height < width * mVideoHeight) { //Log.i("@@@", "image too wide, correcting");
+                    width = height * mVideoWidth / mVideoHeight
+                } else if (mVideoWidth * height > width * mVideoHeight) { //Log.i("@@@", "image too tall, correcting");
+                    height = width * mVideoHeight / mVideoWidth
+                }
+            } else if (widthSpecMode == MeasureSpec.EXACTLY) { // only the width is fixed, adjust the height to match aspect ratio if possible
+                width = widthSpecSize
+                height = width * mVideoHeight / mVideoWidth
+                if (heightSpecMode == MeasureSpec.AT_MOST && height > heightSpecSize) { // couldn't match aspect ratio within the constraints
+                    height = heightSpecSize
+                }
+            } else if (heightSpecMode == MeasureSpec.EXACTLY) { // only the height is fixed, adjust the width to match aspect ratio if possible
+                height = heightSpecSize
+                width = height * mVideoWidth / mVideoHeight
+                if (widthSpecMode == MeasureSpec.AT_MOST && width > widthSpecSize) { // couldn't match aspect ratio within the constraints
+                    width = widthSpecSize
+                }
+            } else { // neither the width nor the height are fixed, try to use actual video size
+                width = mVideoWidth
+                height = mVideoHeight
+                if (heightSpecMode == MeasureSpec.AT_MOST && height > heightSpecSize) { // too tall, decrease both width and height
+                    height = heightSpecSize
+                    width = height * mVideoWidth / mVideoHeight
+                }
+                if (widthSpecMode == MeasureSpec.AT_MOST && width > widthSpecSize) { // too wide, decrease both width and height
+                    width = widthSpecSize
+                    height = width * mVideoHeight / mVideoWidth
+                }
+            }
+        } else { // no size yet, just adopt the given spec sizes
+        }
+        measuredWidth = width
+        measuredHeight = height
+    }
+
+    fun setAspectRatio(aspectRatio: Int) {
+        logD(getAspectRatioText(aspectRatio = aspectRatio))
+        mCurrentAspectRatio = aspectRatio
+    }
+
+
+    init {
+        mWeakView = WeakReference(view)
+    }
+
+    companion object {
+        fun getAspectRatioText(aspectRatio: Int) = when (aspectRatio) {
+            AspectRatio.AR_ASPECT_FIT_PARENT -> "Aspect / Fit parent"
+            AspectRatio.AR_ASPECT_FILL_PARENT -> "Aspect / Fill parent"
+            AspectRatio.AR_ASPECT_WRAP_CONTENT -> "Aspect / Wrap conten"
+            AspectRatio.AR_MATCH_PARENT -> "Free / Fill parent"
+            AspectRatio.AR_16_9_FIT_PARENT -> "16:9 / Fit parent"
+            AspectRatio.AR_4_3_FIT_PARENT -> "4:3 / Fit parent"
+            else -> "NA"
+        }
+    }
+}
