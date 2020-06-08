4d6d2bbec7e9d0fd1fa99aa7a504a022d778d717
tainzhi <qfq61@qq.com>
2020-06-09 06:52:29 +0800

    get douyu room id success

--- MediaSpider/src/main/java/com/tanzhi/mediaspider/Douyu.kt
+++ MediaSpider/src/main/java/com/tanzhi/mediaspider/Douyu.kt
@@ -6,6 +6,7 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
+import org.json.JSONObject
 import java.lang.System.currentTimeMillis
 import java.security.MessageDigest
 import java.time.LocalDateTime
@@ -16,6 +17,9 @@ import java.time.format.DateTimeFormatter
  * @mail:        qfq61@qq.com
  * @date:        2020/6/4 16:01
  * @description: 爬取斗鱼直播间直播源
+ * 通过chrome debug工具获取的地址 https://tc-tct.douyucdn2.cn/dyliveflv1/562483rmTwjem0AN_1200p.flv
+ * 其中_1200p 对应清晰度
+ *
  **/
 
 class Douyu() {
@@ -28,53 +32,70 @@ class Douyu() {
         // t3
         val today = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
 
-        getPreUrl("431935", timeSeconds.toString(), requestQueue)
+        getPreUrl("562483", timeSeconds.toString(), requestQueue)
     }
 
-    fun getPreUrl(rid: String, time: String, requestQueue: RequestQueue): String {
+    /**
+     *
+     */
+    fun getPreUrl(rid: String, time: String, requestQueue: RequestQueue) {
         val url = "https://playweb.douyucdn.cn/lapi/live/hlsH5Preview/$rid"
-        val postData = mapOf(
+        val auth = (rid + time).toMD5()
+        val header = mutableMapOf(
+                "content-type" to "application/x-www-form-urlencoded",
+                "rid" to rid,
+                "time" to time,
+                "auth" to auth
+        )
+
+        val postData = mutableMapOf(
                 "rid" to rid,
                 "did" to "10000000000000000000000000001501"
         )
-        val auth = (rid + time).toMD5()
-        // val auth = ("431935" + "1591575186690").toMD5()
-        val jsonRequest = object : StringRequest(Request.Method.POST,
+
+        /** 成功后的response
+        {
+            "error": 0,
+            "msg": "success",
+            "data": {
+                "room_id": 85894,
+                "rtmp_cdn": "ws",
+                "rtmp_url": "https://hlsa.douyucdn.cn/live",
+                "rtmp_live": "85894rmovieChow_550/playlist.m3u8?wsSecret=ed5c28f6e977e03e91036ce561f473ec\\u0026wsTime=1591625515\\u0026token=web-douyu-0-85894-8a352071adadaeb0f1705309d80dd80d\\u0026did=10000000000000000000000000001501\\u0026ver=2018061203\\u0026st=0\\u0026preview=1\\u0026pt=3\\u0026origin=all",
+                "client_ip": "222.67.156.84",
+                ...
+            }
+        }
+        */
+        val stringRequest = object : StringRequest(Request.Method.POST,
                 url,
                 Response.Listener { response ->
-                    println(response.toString())
+                    val jsonObject = JSONObject(response)
+                    if (jsonObject.getInt("error") == 0) {
+                        val dataObject = jsonObject.getJSONObject("data")
+                        val rtmpLive =dataObject.getString("rtmp_live")
+                        if (rtmpLive.contains("mix=1")) {
+                            println("PKing")
+                        } else {
+                            val regex = Regex(pattern = "[0-9a-zA-Z]*_")
+                            val found = regex.find(rtmpLive)
+                            if (found != null) {
+                                println(found.value)
+                            }
+                        }
+                    }
+                    println("not found")
                 },
                 Response.ErrorListener { error ->
                     println(error.toString())
                 }) {
-            override fun getHeaders(): MutableMap<String, String> {
-                return mutableMapOf(
-                        "content-type" to "application/x-www-form-urlencoded",
-                        "rid" to rid,
-                        "time" to time,
-                        "auth" to auth
-                )
-            }
+            override fun getHeaders() = header
 
-            override fun getBody(): ByteArray {
-                return postData.toString().toByteArray(Charsets.UTF_8) }
+            override fun getParams() = postData
+
         }
-        requestQueue.add(jsonRequest)
-        return ""
+        requestQueue.add(stringRequest)
     }
-
-    // fun request(context: Context) {
-    //     val queue = Volley.newRequestQueue(context)
-    //     val jsonRequest = object : StringRequest(Request.Method.POST) {
-    //         override fun getHeaders(): MutableMap<String, String> {
-    //             return super.getHeaders()
-    //         }
-    //
-    //         override fun getBody(): ByteArray {
-    //             return super.getBody()
-    //         }
-    //     }
-    // }
 }
 
 fun String.toMD5(): String {
