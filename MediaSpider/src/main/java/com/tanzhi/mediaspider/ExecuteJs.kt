8e02599c01903020d640e405006fc5b5517b0cff
tainzhi <qfq61@qq.com>
2020-06-09 18:27:52 +0800

    add js executor

---
+++ MediaSpider/src/main/java/com/tanzhi/mediaspider/ExecuteJs.kt
@@ -0,0 +1,14 @@
+package com.tanzhi.mediaspider
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/6/9 17:51
+ * @description:
+ **/
+
+class ExecuteJs {
+    fun run() {
+        val rhino = Context.
+    }
+}
