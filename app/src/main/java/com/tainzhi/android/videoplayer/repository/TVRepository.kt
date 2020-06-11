981d09f4a0fc875fdfa1d657ca0a712f5ce1448d
tainzhi <qfq61@qq.com>
2020-06-11 17:49:18 +0800

    use TVSpider

--- app/src/main/java/com/tainzhi/android/videoplayer/repository/TVRepository.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/repository/TVRepository.kt
@@ -3,6 +3,7 @@ package com.tainzhi.android.videoplayer.repository
 import com.tainzhi.android.common.base.BaseRepository
 import com.tainzhi.android.videoplayer.bean.Tv
 import com.tainzhi.android.videoplayer.db.TvDao
+import com.tanzhi.mediaspider.TVSpider
 
 /**
  * @author:      tainzhi
@@ -15,5 +16,10 @@ class TVRepository(private val tvDao: TvDao): BaseRepository() {
 
     fun loadTvs(): List<Tv> {
         return tvDao.getAllTv()
+    }
+
+    fun loadTvProgram(): Map<String, String> {
+        return TVSpider.getInstance().getTvProgram("cctv") +
+                TVSpider.getInstance().getTvProgram("satellite")
     }
 }
