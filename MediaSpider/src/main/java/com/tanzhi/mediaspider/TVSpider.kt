9adc8c31828a809414c2ddc95885440446acab01
tainzhi <qfq61@qq.com>
2020-06-11 17:49:18 +0800

    use TVSpider

--- MediaSpider/src/main/java/com/tanzhi/mediaspider/TVSpider.kt
+++ MediaSpider/src/main/java/com/tanzhi/mediaspider/TVSpider.kt
@@ -24,16 +24,22 @@ class TVSpider {
      <td class="pt15 font16 td3"><a href="/tvcolumn/MlIi" title="第一动画乐园" tp="12" res="2020-06-11 17:03_MlIi">第一动画乐园-2020-446</a><br><span class="block mt8 mb12 font14" style="color:#999">17:03</span></td>
     </tr>
      */
+    /**
+     * 从 [https://www.tvmao.com/program/playing/cctv]
+     * 获取各卫视频道当前直播节目
+     *
+     * @param type cctv 或者 satellite, 即央视和地方卫视
+     */
     fun getTvProgram(type: String): Map<String, String> {
         val response = KRequest().get("https://www.tvmao.com/program/playing/$type/")
         val doc = Jsoup.parse(response)
         val tvProgramMap = hashMapOf<String, String>()
         doc.select("tr").forEach { item ->
-            // 获取 /program/CCTV/CCTV1
+            // 获取 /program/CCTV/CCTV1, 正则表达式捕获组 捕获1组
             val href = item.select("tr")[0].select("a")[0].attr("href")
             val key = (".*/(.*)").toRegex().findAll(href).first().groupValues[1]
             val program = item.select("td.pt15")[0].select("[href]").text()
-            tvProgramMap.put(key, program)
+            tvProgramMap[key] = program
         }
         return tvProgramMap
     }
