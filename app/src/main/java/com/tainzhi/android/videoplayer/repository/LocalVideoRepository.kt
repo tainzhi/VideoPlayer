52a836b76e3c240306d1b41d6b7d54272aa243e8
tainzhi <qfq61@qq.com>
2020-06-12 17:40:48 +0800

    LocalVideoFragment功能完成

--- app/src/main/java/com/tainzhi/android/videoplayer/repository/LocalVideoRepository.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/repository/LocalVideoRepository.kt
@@ -31,30 +31,26 @@ class LocalVideoRepository: BaseRepository() {
         App.CONTEXT.contentResolver.query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
                 videoColumnsProjection, null, null, null)?.
                 use { cursor ->
-                    var count = 1
+                    // 不要使用 getColumnIndexOrThrow(), 因为某些columen会抛出异常, 导致循环退出, 而不能查询所有的条目
                     while (cursor.moveToNext()) {
-                        if (++count > 5) {
-                            break
-                        }
-                        val id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID))
+                        val id = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media._ID))
                         val contentUri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id)
                         // 视频大小是以B为单位的整数数字, 故故需转成10KB, 10MB, 10GB
-                        val _size = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE))
+                        val _size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE))
                         val size = FormatUtil.formatMediaSize(_size)
-                        val title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.TITLE))
+                        val title = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE))
                         // 视频大小是以秒(s)为单位的整数数字, 故需转成00:00:00
-                        val duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION))
-                        val resolution = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.RESOLUTION))
-                        // TODO: 2020/6/12 通过resolution判断orientation 
+                        val duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION))
+                        val resolution = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.RESOLUTION))
+                        // TODO: 2020/6/12 通过resolution判断orientation
                         val orientation = "0"
-                        val dateAdded = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED))
-                        val dateModified = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_MODIFIED))
-                        val dateTaken = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_TAKEN))
-                        
-                        list += LocalVideo(id, contentUri, size, title,duration, resolution, orientation,dateTaken,dateAdded, dateModified)
+                        val dateAdded = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATE_ADDED))
+                        val dateModified = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATE_MODIFIED))
+                        val dateTaken = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATE_TAKEN))
+
+                        list += LocalVideo(contentUri, size, title,duration, resolution, orientation,dateTaken,dateAdded, dateModified)
                     }
-                    return list
                 }
-        return emptyList()
+        return list
     }
 }
