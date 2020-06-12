3ebf4ef73e426d3bd62a46cabaabbf51faadabb2
tainzhi <qfq61@qq.com>
2020-06-12 16:39:09 +0800

    add LocalVideoFragment

---
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/local/LocalVideoFragment.kt
@@ -0,0 +1,40 @@
+package com.tainzhi.android.videoplayer.ui.local
+
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.qfq.tainzhi.videoplayer.R
+import com.qfq.tainzhi.videoplayer.databinding.LocalVideoFragmentBinding
+import com.tainzhi.android.common.base.ui.BaseVMFragment
+import com.tainzhi.android.videoplayer.adapter.LocalVideoAdatper
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+
+/**
+ * 本地视频
+ */
+class LocalVideoFragment : BaseVMFragment<LocalVideoViewModel>(useBinding = true) {
+
+    private val localVideoAdapter by lazy(LazyThreadSafetyMode.NONE){ LocalVideoAdatper() }
+
+    override fun getLayoutResId() = R.layout.local_video_fragment
+
+    override fun initVM() = getViewModel<LocalVideoViewModel>()
+
+    override fun initView() {
+        (mBinding as LocalVideoFragmentBinding).localVideoRecyclerView.run {
+            layoutManager = LinearLayoutManager(requireActivity())
+            adapter = localVideoAdapter
+        }
+    }
+
+    override fun initData() {
+        mViewModel.getLocalVideos()
+    }
+
+    override fun startObserve() {
+        mViewModel.apply {
+            localVideoList.observe(viewLifecycleOwner, Observer {it ->
+                localVideoAdapter.setList(it)
+            })
+        }
+    }
+}
