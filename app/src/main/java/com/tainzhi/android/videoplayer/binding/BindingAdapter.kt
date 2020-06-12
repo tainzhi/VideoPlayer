b23a115cfc86bede4d954cc9b3b010cf250462b9
tainzhi <qfq61@qq.com>
2020-06-13 07:24:16 +0800

    create thumbnail in binding adapter

--- app/src/main/java/com/tainzhi/android/videoplayer/binding/BindingAdapter.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/binding/BindingAdapter.kt
@@ -1,9 +1,16 @@
 package com.tainzhi.android.videoplayer.binding
 
+import android.content.ContentUris
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Size
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.tainzhi.android.videoplayer.App
 
 /**
  * @author:      tainzhi
@@ -14,18 +21,26 @@ import com.bumptech.glide.Glide
 
 
 @BindingAdapter(
-        "thumbnail",
+        "videoUri",
         "thumbnailWidth",
         "thumbnailHeight",
         requireAll = false
 )
 fun bindVideoThumbnail(
         imageView: ImageView,
-        thumbnail: Bitmap?,
+        videoUri: Uri,
         width: Int? = null,
         height: Int? = null
 ) {
-    if (thumbnail == null) return
+    var thumbnail: Bitmap? = null
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        thumbnail =
+                App.CONTEXT.contentResolver.loadThumbnail(
+                        videoUri, Size(320, 240), null)
+    } else {
+        thumbnail = MediaStore.Video.Thumbnails.getThumbnail(App.CONTEXT.contentResolver, ContentUris.parseId(videoUri), MediaStore.Images.Thumbnails.MICRO_KIND,
+                BitmapFactory.Options())
+    }
     Glide.with(imageView.context).load(thumbnail)
             .into(imageView)
 }
