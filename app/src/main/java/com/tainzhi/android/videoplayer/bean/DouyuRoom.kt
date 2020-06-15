b34e30757512381a2ccbc680c98c25d3ced58570
tainzhi <qfq61@qq.com>
2020-06-15 17:50:30 +0800

    feat: Douyu直播

---
+++ app/src/main/java/com/tainzhi/android/videoplayer/bean/DouyuRoom.kt
@@ -0,0 +1,21 @@
+package com.tainzhi.android.videoplayer.bean
+
+import java.io.Serializable
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/6/15 16:43
+ * @description: 某个斗鱼game下的房间
+ **/
+ 
+data class DouyuRoom(
+    val room_id: Int,
+    val room_src: String,
+    val room_name: String,
+    val owner_uid: String,
+    val online: Int,
+    val hn: Int,
+    val nickname: String,
+    val url: String
+): Serializable
