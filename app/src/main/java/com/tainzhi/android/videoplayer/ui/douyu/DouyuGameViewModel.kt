64defe7ca7597a963612af38ce9853280fab42db
tainzhi <qfq61@qq.com>
2020-06-15 20:23:51 +0800

    feat: Douyu直播

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/douyu/DouyuGameViewModel.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/douyu/DouyuGameViewModel.kt
@@ -1,7 +1,36 @@
 package com.tainzhi.android.videoplayer.ui.douyu
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.tainzhi.android.common.CoroutinesDispatcherProvider
+import com.tainzhi.android.common.base.Result
 import com.tainzhi.android.common.base.ui.BaseViewModel
+import com.tainzhi.android.videoplayer.bean.DouyuRoom
+import com.tainzhi.android.videoplayer.repository.DouyuRepository
+import kotlinx.coroutines.withContext
 
-class DouyuGameViewModel : BaseViewModel() {
-    // TODO: Implement the ViewModel
+class DouyuGameViewModel(private val douyuRepository: DouyuRepository,
+                         private val coroutinesDispatcherProvider: CoroutinesDispatcherProvider
+) : BaseViewModel() {
+
+    private val _rooms =  MutableLiveData<List<DouyuRoom>>()
+    val rooms: LiveData<List<DouyuRoom>>
+        get() = _rooms
+
+
+    fun getGameRooms(gameId: String) {
+        launch {
+            val result = douyuRepository.getGameRooms(gameId)
+            if (result is Result.Success) {
+                emitData(result.data)
+            }
+        }
+    }
+
+    private suspend fun emitData(data: List<DouyuRoom>) {
+        withContext(coroutinesDispatcherProvider.main) {
+            _rooms.value = data
+        }
+    }
+
 }
