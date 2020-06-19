e2f8c37a08be701fa2cd20f886b4bbc018b0f9c6
tainzhi <qfq61@qq.com>
2020-06-19 11:06:08 +0800

    优化卫视节目加载逻辑

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/tv/TVViewModel.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/tv/TVViewModel.kt
@@ -15,6 +15,9 @@ class TVViewModel(private val tvRepository: TVRepository,
     private val _tvList: MutableLiveData<List<Tv>> = MutableLiveData()
     val tvList: LiveData<List<Tv>>
         get() = _tvList
+    private val _tvPrograms = MutableLiveData<Map<String, String>>()
+    val tvPrograms: LiveData<Map<String, String>>
+        get() = _tvPrograms
 
     /**
      * 从数据库获取卫视列表
@@ -31,13 +34,15 @@ class TVViewModel(private val tvRepository: TVRepository,
      */
     fun getTVProgram() {
         launch {
-            val result = tvRepository.loadTVProgram()
+            val tvPrograms = tvRepository.loadTVProgram()
+            _tvPrograms.postValue(tvPrograms)
         }
     }
 
     /**
      * 从本地数据库获取卫视列表的同时, 请求网络电视猫获取改卫视正在播放的节目
-     * todo: 等效于同步实现. 更好的方式是在Adapter中每隔固定时间请求网络, 自动更新该卫视的正在直播节目
+     * 从本地读取tv列表比网络请求获取tv正在播放的节目快很多
+     * 不能把两者请求结果同步处理
      */
     fun getTVListAndProgram() {
         launch {
