7d28aa2c58b6244abca9eeecb9c1f8097de15704
tainzhi <qfq61@qq.com>
2020-06-20 10:33:17 +0800

    feat: delete video for android 10

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/local/LocalVideoViewModel.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/local/LocalVideoViewModel.kt
@@ -1,6 +1,10 @@
 package com.tainzhi.android.videoplayer.ui.local
 
+import android.app.RecoverableSecurityException
+import android.content.IntentSender
 import android.net.Uri
+import android.os.Build
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tainzhi.android.common.CoroutinesDispatcherProvider
 import com.tainzhi.android.common.base.ui.BaseViewModel
@@ -11,6 +15,11 @@ import kotlinx.coroutines.withContext
 class LocalVideoViewModel(private val localVideoRepository: LocalVideoRepository,
                           private val dispatcherProvider: CoroutinesDispatcherProvider
 ) : BaseViewModel() {
+    private var pendingDeleteVideoUri: Uri? = null
+
+    private val _permissionNeededForDelete = MutableLiveData<IntentSender?>()
+    val permissionNeededForDelete: LiveData<IntentSender?> = _permissionNeededForDelete
+
     private val _localVideoList = MutableLiveData<List<LocalVideo>>()
     val localVideoList
         get() = _localVideoList
@@ -25,8 +34,32 @@ class LocalVideoViewModel(private val localVideoRepository: LocalVideoRepository
     fun deleteVideo(uri: Uri) {
         launch {
             withContext(dispatcherProvider.io) {
-                localVideoRepository.deleteVideo(uri)
+                try {
+                    localVideoRepository.deleteVideo(uri)
+                } catch (securityException: SecurityException) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        val recoverableSecurityException =
+                                securityException as? RecoverableSecurityException
+                                        ?: throw securityException
+
+                        // Signal to the Activity that it needs to request permission and
+                        // try the delete again if it succeeds.
+                        pendingDeleteVideoUri = uri
+                        _permissionNeededForDelete.postValue(
+                                recoverableSecurityException.userAction.actionIntent.intentSender
+                        )
+                    } else {
+                        throw securityException
+                    }
+                }
             }
+        }
+    }
+
+    fun deletePendingVideo() {
+        pendingDeleteVideoUri?.let { uri ->
+            pendingDeleteVideoUri = null
+            deleteVideo(uri)
         }
     }
 
