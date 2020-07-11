27f849e021f1dab269108a3c14159a7bd779c487
tainzhi <qfq61@qq.com>
2020-07-11 11:59:04 +0800

    feat: 添加普通弹幕

---
+++ danmu/src/main/java/com/tanzhi/android/danmu/DanmuBean.kt
@@ -0,0 +1,17 @@
+package com.tanzhi.android.danmu
+
+import java.io.Serializable
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/7/11 05:52
+ * @description:
+ **/
+ 
+data class DanmuBean(
+        val id: Int,
+        val avatorId: Int,
+        val userName: String,
+        val content: String
+): Serializable
