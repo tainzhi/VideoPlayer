c454c805a39feda5e411ef5515ee9580a5b6db48
tainzhi <qfq61@qq.com>
2020-08-10 23:22:31 +0800

    feat: Pusher native code

---
+++ ffmpeg/src/main/cpp/pusher/pusher-lib.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>>
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tainzhi_android_ffmpeg_PusherManager_nativeInit(JNIEnv *env, jobject thiz,
+                                                         jboolean is_media_codec) {
+    // TODO: implement nativeInit()
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_tainzhi_android_ffmpeg_PusherManager_nativePUshVideo(JNIEnv *env, jobject thiz,
+                                                              jobjectArray data) {
+    // TODO: implement nativePUshVideo()
+}
