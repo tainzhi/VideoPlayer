095d20149eb2f517a2dd7bea1729a57073e8db96
tainzhi <qfq61@qq.com>
2020-08-11 23:21:37 +0800

    feat: add pusher jni

--- ffmpeg/src/main/java/com/tainzhi/android/ffmpeg/PusherManager.kt
+++ ffmpeg/src/main/java/com/tainzhi/android/ffmpeg/PusherManager.kt
@@ -17,6 +17,12 @@ class PusherManager {
     }
 
     external fun nativeInit(isMediaCodec: Boolean)
-
-    external fun nativePUshVideo(data: Array<Byte>)
+    external fun nativeStart(path: String)
+    external fun nativePushVideo(data: Array<Byte>)
+    external fun nativeSetVideoEncoderInfo(width: Int, height: Int, fps: Int, bit: Int)
+    external fun nativeSetAudioEncoderInfo(sampleRate: Int, channels: Int)
+    external fun nativePushAudio(data: Array<Byte>)
+    external fun pushH264(h264Data: Array<Byte>, type: Int, timeStamp: Int)
+    external fun pushAACData(audio: Array<Byte>, length: Int, timeStamp: Int)
+    external fun nativeRelease()
 }
