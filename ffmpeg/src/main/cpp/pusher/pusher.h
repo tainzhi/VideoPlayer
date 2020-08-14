074ad977bca90589ffb89b0ef793aa4dffde7553
tainzhi <qfq61@qq.com>
2020-08-14 23:37:09 +0800

    add some native rtmp code

---
+++ ffmpeg/src/main/cpp/pusher/pusher.h
@@ -0,0 +1,42 @@
+//
+// Created by muqing on 2020/8/14.
+// Email: qfq61@qq.com
+//
+
+#ifndef VIDEOPLAYER_PUSHER_H
+#define VIDEOPLAYER_PUSHER_H
+
+#include <jni.h>
+#include "PushCallback.h"
+#include "RTMPModel.h"
+#include "../../../../../../../Library/Android/sdk/ndk/20.1.5948944/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/jni.h"
+
+class Pusher {
+public:
+    RTMPModel *rtmpModel = 0;
+    
+    int IsStart();
+    
+    int IsReadyPushing();
+    
+    void init(JavaVM *javaVm, JNIEnv *env, jobject instance) {
+        PushCallback *pushCallback = new PushCallback(javaVm, env, instance);
+        RTMPModel rtmpModel = new RTMPModel(pushCallback);
+        
+        this->rtmpModel = rtmpModel;
+    }
+    
+    void release() {
+        if (rtmpModel) {
+            rtmpModel->release();
+            delete  rtmpModel;
+            rtmpModel = nullptr;
+        }
+    }
+    
+    void start(const char *path) {
+        rtmpModel->_onConnect(path);
+    }
+};
+
+#endif //VIDEOPLAYER_PUSHER_H
