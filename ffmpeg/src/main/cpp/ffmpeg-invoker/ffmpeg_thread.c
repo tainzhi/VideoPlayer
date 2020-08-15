7b44f63fd798f7cb15b1364579bdc96cc117b6f2
tainzhi <qfq61@qq.com>
2020-08-15 23:27:30 +0800

    refactor ffmpeg invoker

--- ffmpeg/src/main/cpp/ffmpeg-invoker/ffmpeg_thread.c
+++ ffmpeg/src/main/cpp/ffmpeg-invoker/ffmpeg_thread.c
@@ -3,7 +3,7 @@
 #include "libavcodec/avcodec.h"
 #include "include/ffmpeg_thread.h"
 
-#define TAG "ffmpeg-thread"
+#define TAG "ffmpeg_thread"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
