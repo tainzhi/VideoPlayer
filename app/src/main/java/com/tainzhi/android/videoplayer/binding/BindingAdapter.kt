f9627bb675d9eeca8c1cc01be3fb996864c7b647
tainzhi <qfq61@qq.com>
2020-09-06 23:07:02 +0800

    fix：VideoThumbnail显示优化

--- app/src/main/java/com/tainzhi/android/videoplayer/binding/BindingAdapter.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/binding/BindingAdapter.kt
@@ -5,20 +5,27 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.ThumbnailUtils
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+import android.util.Size
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.browser.customtabs.CustomTabsService
 import androidx.core.content.ContextCompat
+import androidx.core.net.toFile
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomViewTarget
+import com.bumptech.glide.request.target.ImageViewTarget
+import com.bumptech.glide.request.target.ViewTarget
+import com.google.android.material.tabs.TabLayout
 import com.tainzhi.android.common.util.FormatUtil.formatMediaDuration
 import com.tainzhi.android.videoplayer.App
 import com.tainzhi.android.videoplayer.R
@@ -47,20 +54,21 @@ fun bindVideoThumbnail(
         width: Int? = null,
         height: Int? = null
 ) {
-    lateinit var thumbnail: Bitmap
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        thumbnail =
+    val thumbnail: Bitmap? =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 App.CONTEXT.contentResolver.loadThumbnail(
                         videoUri, Size(320, 240), null)
-    } else {
-        thumbnail = MediaStore.Video.Thumbnails.getThumbnail(App.CONTEXT.contentResolver, ContentUris.parseId(videoUri), MediaStore.Images.Thumbnails.MICRO_KIND,
-                BitmapFactory.Options())
-    }
-    // TODO: 2020/7/9 小米9 android10上file not found exception
-    // thumbnail = MediaStore.Video.Thumbnails.getThumbnail(App.CONTEXT.contentResolver, ContentUris.parseId(videoUri), MediaStore.Images.Thumbnails.MICRO_KIND,
-    //         BitmapFactory.Options())
+            } else {
+                // val file = videoUri.toFile()
+                // android 29 才能使用
+                // ThumbnailUtils.createVideoThumbnail(videoUri.toFile(), Size(320, 240), null)
+                MediaStore.Video.Thumbnails.getThumbnail(App.CONTEXT.contentResolver,
+                        ContentUris.parseId(videoUri),
+                        MediaStore.Images.Thumbnails.MINI_KIND,
+                        BitmapFactory.Options().apply { inSampleSize = 4 })
+            }
     val options = RequestOptions()
-    placeHolder?.let {options.placeholder(placeHolder)}
+    placeHolder?.let { options.placeholder(placeHolder) }
     cornerRadius?.let { options.transform(RoundedCorners(it)) }
     Glide.with(imageView.context).load(thumbnail)
             .apply(options)
@@ -68,7 +76,7 @@ fun bindVideoThumbnail(
 }
 
 @BindingAdapter(
-    "duration"
+        "duration"
 )
 fun bindDuration(textView: TextView, duration: Long) {
     textView.text = duration.formatMediaDuration()
