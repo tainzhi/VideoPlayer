0c1644ab5cf2c8de90a8b8bc7460e51b013e82f8
tainzhi <qfq61@qq.com>
2020-09-09 22:06:13 +0800

    feat: 开始编写DanmuView

---
+++ danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/DanmuConsumer.kt
@@ -0,0 +1,12 @@
+package com.tanzhi.android.danmu.advancedanmu
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/9/9 下午9:38
+ * @description:
+ **/
+
+class DanmuConsumer: Thread() {
+
+}
---
+++ danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/view/IDanMu.kt
@@ -0,0 +1,13 @@
+package com.tanzhi.android.danmu.advancedanmu.view
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/9/9 下午9:54
+ * @description:
+ **/
+
+interface IDanMu {
+    fun hasCanTouchDanmus(): Boolean
+
+}
