2c79ec77f02db09c0022b904b719110708792e63
tainzhi <qfq61@qq.com>
2020-09-10 20:52:13 +0800

    feat: DanmuView

---
+++ danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/DanmuController.kt
@@ -0,0 +1,13 @@
+package com.tanzhi.android.danmu.advancedanmu
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/9/10 20:44
+ * @description:
+ **/
+
+class DanmuController {
+    private val danmuPoolManager = DanmuPoolManager()
+    private val danmu
+}
