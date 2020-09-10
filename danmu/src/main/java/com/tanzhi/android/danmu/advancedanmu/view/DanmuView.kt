7865ef19c3f1ed531dc1e7e6d2c243dff2525812
tainzhi <qfq61@qq.com>
2020-09-10 20:52:13 +0800

    feat: DanmuView

--- danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/view/DanmuView.kt
+++ danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/view/DanmuView.kt
@@ -1,5 +1,11 @@
 package com.tanzhi.android.danmu.advancedanmu.view
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.tanzhi.android.danmu.advancedanmu.DanmuController
+import com.tanzhi.android.danmu.advancedanmu.DanmuModel
+
 /**
  * @author:      tainzhi
  * @mail:        qfq61@qq.com
@@ -7,5 +13,50 @@ package com.tanzhi.android.danmu.advancedanmu.view
  * @description:
  **/
 
-class DanmuView: IDanmu {
+class DanmuView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr), IDanmu {
+
+    private val danmuController = DanmuController()
+    private val onDanmuViewTouchListeners = mutableListOf<OnDanmuViewTouchListener>()
+    private val onDanmuParentViewTouchCallbackListener: OnDanmuParentViewTouchCallbackListener? = null
+
+    private var drawFinished = false
+
+    fun prepare() {
+        danmuController.setSpeedController
+        danmuController.prepare()
+    }
+
+    override fun add(danmuModel: DanmuModel) {
+        danmuController.addDanmuView(-1, danmuModel)
+    }
+
+    override fun add(index: Int, danmuModel: DanmuModel) {
+        TODO("Not yet implemented")
+    }
+
+    override fun jumpQueue(danmuViews: List<DanmuModel>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun lockDraw() {
+        TODO("Not yet implemented")
+    }
+
+    override fun forceSleep() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideAllDanmuView(hideAll: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideNormalDanmu(hide: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasCanTouchDanmus(): Boolean {
+        TODO("Not yet implemented")
+    }
 }
