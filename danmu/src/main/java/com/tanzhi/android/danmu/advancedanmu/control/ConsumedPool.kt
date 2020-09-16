807ac441110b658bafaa0641f77f6b94ce68f137
tainzhi <qfq61@qq.com>
2020-09-16 22:54:35 +0800

    feat: 开始编写DanmuView

--- danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/control/ConsumedPool.kt
+++ danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/control/ConsumedPool.kt
@@ -2,7 +2,10 @@ package com.tanzhi.android.danmu.advancedanmu.control
 
 import android.content.Context
 import com.tanzhi.android.danmu.advancedanmu.DanmuModel
+import com.tanzhi.android.danmu.advancedanmu.control.speed.ISpeedController
 import com.tanzhi.android.danmu.advancedanmu.painter.DanmuPainter
+import com.tanzhi.android.danmu.advancedanmu.painter.L2RPainter
+import com.tanzhi.android.danmu.advancedanmu.painter.R2LPainter
 
 /**
  * @author:      tainzhi
@@ -20,6 +23,27 @@ class ConsumedPool(context: Context) {
     val danmuPainterMap = hashMapOf<Int, DanmuPainter>()
     @Volatile
     var mixedDanmuViewQueue = arrayListOf<DanmuModel>()
+
+    var speedController: ISpeedController? = null
     
-    private var isDrawing = false`
+    private var isDrawing: Boolean = false
+
+    init {
+        danmuPainterMap[DanmuModel.LEFT_TO_RIGHT] = L2RPainter()
+        danmuPainterMap[DanmuModel.RIGHT_TO_LEFT] = R2LPainter()
+        hide(false)
+    }
+
+
+    private fun hide(hide: Boolean) {
+        danmuPainterMap.forEach { (_, u) ->
+            u.hide = hide
+        }
+    }
+
+    private fun hideAll(hide: Boolean) {
+        danmuPainterMap.forEach { (_, u) ->
+            u.hideAll = hide
+        }
+    }
 }
