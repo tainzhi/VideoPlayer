c99bf54edfd0717b80f7b747c19868cae409de0b
tainzhi <qfq61@qq.com>
2020-09-19 23:23:36 +0800

    feat: 编写DanmuView, Producer

--- danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/control/Producer.kt
+++ danmu/src/main/java/com/tanzhi/android/danmu/advancedanmu/control/Producer.kt
@@ -1,5 +1,9 @@
 package com.tanzhi.android.danmu.advancedanmu.control
 
+import android.os.Handler
+import android.os.Message
+import com.tanzhi.android.danmu.advancedanmu.DanmuModel
+
 /**
  * @author:      tainzhi
  * @mail:        qfq61@qq.com
@@ -7,5 +11,61 @@ package com.tanzhi.android.danmu.advancedanmu.control
  * @description:
  **/
 
-class Producer {
+class Producer(val producedPool: ProducedPool, val consumedPool: ConsumedPool) {
+    private lateinit var producerHandler: ProducerHandler
+    fun start() {
+        producerHandler = ProducerHandler(this)
+    }
+
+    fun produce(index: Int, danmuModel: DanmuModel) {
+        val produceMessage = ProduceMessage(index, danmuModel)
+        val message = producerHandler.obtainMessage().apply {
+            obj = produceMessage
+            what = 2
+        }
+        producerHandler.sendMessage(message)
+    }
+
+    fun jumpQueue(danmuModels: List<DanmuModel>) { producedPool.jumpQueue(danmuModels)}
+
+    fun release() {
+        producerHandler.run {
+            removeMessages(1)
+            release()
+        }
+
+    }
 }
+
+class ProducerHandler(val producer: Producer): Handler() {
+    companion object {
+        const val SLEEP_TIME = 100L
+    }
+    init {
+        obtainMessage(1).sendToTarget()
+    }
+
+    override fun handleMessage(msg: Message) {
+        super.handleMessage(msg)
+        when(msg.what) {
+            1 -> {
+                producer.producedPool.dispatch()?.let {it ->
+                    producer.consumedPool.put(it)
+                }
+                val message = obtainMessage().apply { what = 1 }
+                sendMessageDelayed(message, SLEEP_TIME)
+            }
+            2 -> {
+                val producerMessage = msg.obj as ProduceMessage
+                producer.producedPool.addDanmu(producerMessage.index, producerMessage.danmuModel)
+            }
+        }
+    }
+
+    fun release() {
+        producer.producedPool.clear()
+    }
+}
+
+data class ProduceMessage(var index: Int, val danmuModel: DanmuModel)
+
