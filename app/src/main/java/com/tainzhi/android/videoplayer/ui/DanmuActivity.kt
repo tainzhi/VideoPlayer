0f42842b8b99bbc6aaf0c0276a5223d7b6956fd7
tainzhi <qfq61@qq.com>
2020-10-12 21:24:11 +0800

    add Danmu fake datas

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/DanmuActivity.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/DanmuActivity.kt
@@ -24,9 +24,10 @@ class DanmuActivity : AppCompatActivity() {
                 DanmuEntity(
                     arrayOf(
                         "https://q.qlogo.cn/qqapp/100229475/E573B01150734A02F25D8E9C76AFD138/100",
-                        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602480953251&di=2904f20dcc633e1e901d65d78a06a372&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fcd56c51010f9edb8b17ef441e6561cf471b511f22627-vCre39_fw658",
+                        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602513751308&di=4750ba99977f4478144d0790b6857069&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_origin_min_pic%2F00%2F73%2F65%2F0656df6b43bacf5.jpg",
                         "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602481010486&di=1eea4306e070ce336e0c28f82480613a&imgtype=0&src=http%3A%2F%2Fp.geitu.net%2F409%2F5P-jvh.jpg%3Fx-oss-process%3Dimage%2Fresize%2Cw_256%2Climit_1",
                         "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602481030402&di=a510961dbd68fe447679c54c29e677b2&imgtype=0&src=http%3A%2F%2Favatar.gxnews.com.cn%2Favatar%2F000%2F08%2F33%2F51_avatar_big.jpg",
+                        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=781934719,1232780622&fm=15&gp=0.jpg",
                         "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602481030258&di=b485992c8565f1123baa9e98889a867e&imgtype=0&src=http%3A%2F%2Fbbsimg.0411cxd.com%2Fforum%2F201105%2F03%2F203852cp0q2mzlq92mps43.png"
                     ).random(),
                     arrayOf("张三", "李四", "王五", "老板boss", "至尊大使", "尊贵皇希特").random(),
@@ -35,7 +36,7 @@ class DanmuActivity : AppCompatActivity() {
                     1,
                     arrayOf(
                         "欲穷千里目, 更上一层楼~~~",
-                        "6666",
+                        "666666666666666666666666666666666666666666666666666666666666666666666666666666666666",
                         "sb fgsb fgsb fgsb fgsb fgsb fgsb",
                         "nbbbbbbbbbbbbility",
                         "fgsbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
