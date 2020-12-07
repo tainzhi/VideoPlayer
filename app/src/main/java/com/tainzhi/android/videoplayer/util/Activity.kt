ba5dcde8b7b7faf966a730492d49e46e2f4983ed
tainzhi <qfq61@qq.com>
2020-12-07 17:27:27 +0800

    feat(util): add Activity Extension

---
+++ app/src/main/java/com/tainzhi/android/videoplayer/util/Activity.kt
@@ -0,0 +1,28 @@
+package com.tainzhi.android.videoplayer.util
+
+import android.app.ActivityManager
+import android.content.Context
+
+/**
+ * File:     Activity
+ * Author:   tainzhi
+ * Created:  2020/12/7 10:12
+ * Mail:     QFQ61@qq.com
+ * Description:
+ */
+
+/**
+ * 当前activity是否显示, 即在最上面, 用户可见
+ *
+ * getRunningTasks在api>=21已经 Deprecated
+ */
+fun Context.isForeGround(): Boolean {
+    val activityName = this.javaClass.name
+    val activityList = (this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+            .getRunningTasks(1)
+    if (!activityList.isNullOrEmpty()) {
+        val componentName = activityList[0].topActivity
+        return componentName?.className == activityName
+    }
+    return false
+}
