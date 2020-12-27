6036848dcb329e93fbab5d086ebaeff61b27a19b
tainzhi <qfq61@qq.com>
2020-12-27 17:30:16 +0800

    feat(Movie): 电影搜索功能完成

--- MediaSpider/src/main/java/com/tainzhi/mediaspider/film/bean/SearchResult.kt
+++ MediaSpider/src/main/java/com/tainzhi/mediaspider/film/bean/SearchResult.kt
@@ -9,9 +9,9 @@ package com.tainzhi.mediaspider.film.bean
  */
 data class SearchResultData(
         //视频id
-        val id: String?,
+        val id: String,
         //名字
-        val name: String?,
+        val name: String,
         //类型，战争片
         val type: String
 )
