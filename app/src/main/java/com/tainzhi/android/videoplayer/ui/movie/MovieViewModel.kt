738a54baad849bb6660dc7c953f1ee7eb545c811
tainzhi <qfq61@qq.com>
2020-12-27 15:40:21 +0800

    fix(Movie): 无法加载更多; 重复加载

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/movie/MovieViewModel.kt
@@ -12,6 +12,7 @@ import com.tainzhi.android.videoplayer.repository.MovieRepository
 import com.tainzhi.mediaspider.film.bean.Classify
 import com.tainzhi.mediaspider.film.bean.DetailData
 import com.tainzhi.mediaspider.film.bean.HomeChannelData
+import com.tainzhi.mediaspider.film.bean.SearchResultData
 import kotlinx.coroutines.launch
 
 /**
@@ -25,9 +26,6 @@ class MovieViewModel(
         private val movieRepository: MovieRepository,
         private val dispatcherProvider: CoroutinesDispatcherProvider,
 ) : ViewModel() {
-
-    // 用于标记Adapter是否重置数据, 还是添加数据
-    var isRefreshLoading = false
 
     private val _classifyLiveData = MutableLiveData<List<Classify>>()
     val classifyListLiveData: LiveData<List<Classify>>
@@ -50,20 +48,13 @@ class MovieViewModel(
         }
     }
 
-    private var channelCurPage = 1
-    fun getChannelData(channelId: String, isRefresh: Boolean = false) {
-        isRefreshLoading = isRefresh
-        if (isRefresh) {
-            channelCurPage = 1
-        } else {
-            channelCurPage++
-        }
+    fun getChannelData(channelId: String, page: Int) {
         viewModelScope.launch(dispatcherProvider.io) {
-            movieRepository.movieManager.curUseSourceConfig().requestHomeChannelData(channelCurPage, channelId) { channelData ->
+            movieRepository.movieManager.curUseSourceConfig().requestHomeChannelData(page, channelId) { channelData ->
                 if (channelData == null) {
                     _channelLiveData.postValue(Result.successEndData(emptyList()))
                 } else {
-                    _channelLiveData.postValue(Result.successEndData(channelData))
+                    _channelLiveData.postValue(Result.success(channelData))
                 }
             }
         }
@@ -88,6 +79,23 @@ class MovieViewModel(
             movieRepository.movieManager.defaultSourceKey = sourceKey
         } else {
             Logger.e("sourceKey is null")
+        }
+    }
+
+    private val _searchResult = MutableLiveData<List<SearchResultData>>()
+    val searchResult: LiveData<List<SearchResultData>>
+        get() = _searchResult
+
+    private var searchCurPage = 1
+
+    // FIXME: 2020/12/26  对于更多的搜索结果, 分页显示
+    fun searchMovie(key: String) {
+        movieRepository.movieManager.curUseSourceConfig().requestSearchData(key, searchCurPage) {
+            viewModelScope.launch(dispatcherProvider.io) {
+                movieRepository.movieManager.curUseSourceConfig().requestSearchData(key, searchCurPage) {
+                    _searchResult.postValue(it)
+                }
+            }
         }
     }
 }
