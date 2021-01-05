668bc299104beeb78f23cdeb6426d79f96a283ef
tainzhi <qfq61@qq.com>
2021-01-05 15:38:57 +0800

    refactor(Splash): 启动页添加动画

--- app/src/main/java/com/tainzhi/android/videoplayer/ui/SplashActivity.kt
+++ app/src/main/java/com/tainzhi/android/videoplayer/ui/SplashActivity.kt
@@ -1,95 +1,34 @@
 package com.tainzhi.android.videoplayer.ui
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.tainzhi.android.videoplayer.widget.dialog.showCheckPermissionDialog
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.tainzhi.android.common.base.ui.BaseViewBindingActivity
+import com.tainzhi.android.common.util.startKtxActivity
+import com.tainzhi.android.videoplayer.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
-
-    companion object {
-        const val REQUEST_REQUIRED_PERMISSIONS = 42
-    }
-
-    private val requestPermissions = arrayOf(
-            Manifest.permission.INTERNET,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    )
-
-    private var isMissingPermissions = true
-    private var showRationale = false
+class SplashActivity : BaseViewBindingActivity<ActivitySplashBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        checkPermission()
-        if (!isMissingPermissions) {
-            navigateToMain()
-        }
-    }
-
-    private fun navigateToMain() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
-    }
-
-    /**
-     * 检查是否缺少权限
-     */
-    private fun checkPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED) {
-            isMissingPermissions = false
-        } else {
-            requestMissingRequiredPermissions()
-        }
-    }
-
-    private fun requestMissingRequiredPermissions() {
-        val missing = HashSet<String>()
-        showRationale = false
-        for (permission in requestPermissions) {
-            if (ContextCompat.checkSelfPermission(this, permission)
-                    != PackageManager.PERMISSION_GRANTED) {
-                missing.add(permission)
-                showRationale = showRationale or (shouldShowRequestPermissionRationale(permission))
+        mBinding.splashMotionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
             }
-        }
-        if (missing.isNotEmpty()) {
-            requestPermissions(missing.toTypedArray(), REQUEST_REQUIRED_PERMISSIONS)
-        }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startKtxActivity<MainActivity>()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_REQUIRED_PERMISSIONS) {
-            var granted = true
-            var _showRationale = false
-
-            for (i in grantResults.indices) {
-                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                    granted = false
-                    _showRationale = _showRationale || shouldShowRequestPermissionRationale(permissions[i])
-
-                }
-            }
-            if (granted) {
-                navigateToMain()
-            } else {
-                // If we were not supposed to show the rationale before requestPermissions(...) and
-                // we still shouldn't show the rationale it means the user previously selected
-                // "don't ask again" in the permission request dialog. In this case we bring up the
-                // system permission settings for this package.
-                if (!_showRationale && !showRationale) {
-                    showCheckPermissionDialog(supportFragmentManager)
-                } else {
-                    finish()
-                }
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
+    override fun onResume() {
+        super.onResume()
+        mBinding.splashMotionLayout.startLayoutAnimation()
     }
 }
