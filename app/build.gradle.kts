5ac7be105985c30ec8c595c4fa5c3648508ccab9
tainzhi <qfq61@qq.com>
2024-11-12 18:21:06 +0800

    build: 更新 KSP 插件版本到2.0.0-1.0.21并统一管理
    - 移除 app、MediaSpider 模块和根目录中 KSP 插件的版本声明
    - 在 gradle/libs.versions.toml 中添加 KSP 插件版本统一管理
    - 更新 build.gradle.kts 文件中的 KSP 插件引用

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+    id("com.google.devtools.ksp")
     id("kotlin-parcelize") // 弃用android.extensions后, 使用parcelize
     id("kotlin-kapt") // databinding 需要, google推荐compose，故databinding已经是维护状态，不再更新
     // kotlin("plugin.serialization")
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,5 +16,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    // id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+    alias(libs.plugins.ksp) apply false
 }
